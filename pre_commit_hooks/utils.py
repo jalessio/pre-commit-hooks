3af7d10137e8814f2c25f398021064fd66eb264f
Jamie Alessio <jamie@calthorpeanalytics.com>
2017-01-10 14:57:36 -0800

    giti submodule compatibility

--- pre_commit_hooks/utils.py
+++ pre_commit_hooks/utils.py
@@ -1,11 +1,17 @@
-# Taken from: http://code.activestate.com/recipes/173220-test-if-a-file-or-string-is-text-or-binary/
+import os
 
+# Taken from: http://code.activestate.com/recipes/173220-test-if-a-file-or-string-is-text-or-binary/
 KNOWN_BINARY_FILE_EXTS = ('.pdf',)
 
 def is_textfile(filename, blocksize=512):
     if any(filename.endswith(ext) for ext in KNOWN_BINARY_FILE_EXTS):
         return False
-    return is_text(open(filename, 'rb').read(blocksize))
+    # git submodules appear here as files even though they are directories
+    # so we just skip over them
+    if os.path.isdir(filename):
+        return False
+    else:
+        return is_text(open(filename, 'rb').read(blocksize))
 
 def is_text(stuff):
     if b"\0" in stuff:
